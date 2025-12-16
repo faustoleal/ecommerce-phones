6d2bb37c399d0351331cbc828b2e022214875f6f
Fausto Leal <fausto.leal2099@gmail.com>
2025-12-16 11:22:15 -0300

    agregando modelo de products

---
+++ src/models/products.ts
@@ -0,0 +1,102 @@
+import { Schema, model } from "mongoose";
+import { Os, Processor, Brands, Product } from "../types/products";
+
+const productsSchema = new Schema<Product>({
+  brand_name: {
+    type: String,
+    enum: Object.values(Brands),
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  has_5g: {
+    type: Boolean,
+    required: true,
+  },
+  has_nfc: {
+    type: Boolean,
+    required: true,
+  },
+  has_ir_blaster: {
+    type: Boolean,
+    required: true,
+  },
+  processor_brand: {
+    type: String,
+    enum: Object.values(Processor),
+    required: true,
+  },
+  num_cores: {
+    type: Number,
+    required: true,
+  },
+  processor_speed: {
+    type: Number,
+    required: true,
+  },
+  battery_capacity: {
+    type: Number,
+    required: true,
+  },
+  fast_charging_available: {
+    type: Boolean,
+    required: true,
+  },
+  ram_capacity: {
+    type: Number,
+    required: true,
+  },
+  internal_memory: {
+    type: Number,
+    required: true,
+  },
+  screen_size: {
+    type: Number,
+    required: true,
+  },
+  num_rear_cameras: {
+    type: Number,
+    required: true,
+  },
+  num_front_cameras: {
+    type: Number,
+    required: true,
+  },
+  os: {
+    type: String,
+    enum: Object.values(Os),
+    required: true,
+  },
+  primary_camera_rear: {
+    type: Number,
+    required: true,
+  },
+  primary_camera_front: {
+    type: Number,
+    required: true,
+  },
+  extended_memory_available: {
+    type: Boolean,
+    required: true,
+  },
+  resolution_width: {
+    type: Number,
+    required: true,
+  },
+  resolution_height: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const ProductModel = model<Product>("Product", productsSchema);

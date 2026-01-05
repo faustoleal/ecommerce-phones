a1dae34b0426fb10d5817fc03c08a986e2e6e48c
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-05 12:29:13 -0300

    agregando FiltrosSection

---
+++ src/components/FilterSection.tsx
@@ -0,0 +1,59 @@
+import { Search } from "lucide-react";
+import Label from "./Label";
+import Input from "./Input";
+
+const FilterSection = () => {
+  return (
+    <div className="space-y-6">
+      {/* Search */}
+      <div className="space-y-2">
+        <Label htmlFor="search">Buscar</Label>
+        <div className="relative">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+          <Input
+            id="search"
+            placeholder="Buscar productos..."
+            className="pl-9"
+          />
+        </div>
+      </div>
+      {/* Marcas */}
+      <div className="space-y-3">
+        <Label>Marca</Label>
+        <div className="flex items-center space-x-2">
+          <input id="apple" type="checkbox" />
+          <Label htmlFor="apple">Apple</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <input id="samsung" type="checkbox" />
+          <Label htmlFor="samsung">Samsung</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <input id="vivo" type="checkbox" className=" py-1 px-3" />
+          <Label htmlFor="vivo">Vivo</Label>
+        </div>
+      </div>
+      {/* Precio */}
+      <div className="space-y-3">
+        <Label htmlFor="price">Precio: $0 - $3000</Label>
+        <input
+          id="price"
+          type="range"
+          min={0}
+          max={3000}
+          step={50}
+          className="flex w-full touch-none items-center select-none py-2 bg-black"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="align-center py-2 w-full bg-transparent font-medium rounded-md border"
+      >
+        Limpiar filtros
+      </button>
+    </div>
+  );
+};
+
+export default FilterSection;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,7 @@
-const Input = ({ ...props }: React.ComponentProps<"input">) => {
+const Input = ({ className, ...props }: React.ComponentProps<"input">) => {
   return (
     <input
-      className="border border-[#E3E6EA] rounded-md h-9 w-full min-w-0 bg-transparent py-1 px-3 text-base shadow-xs outline-none"
+      className={`border border-[#E3E6EA] rounded-md h-9 w-full min-w-0 bg-transparent py-1 px-3 text-base shadow-xs outline-none ${className}`}
       {...props}
     />
   );

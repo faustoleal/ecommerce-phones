b2a309a37151fd87231829013c2c7befbcbae61d
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-07 12:03:52 -0300

    agregando getDestacadosPhones

--- src/controllers/phones.ts
+++ src/controllers/phones.ts
@@ -38,3 +38,23 @@ export async function getPhonesById(id: string) {
     );
   }
 }
+
+export async function getDestacadosPhones() {
+  try {
+    const [apple, samsung, xiaomi] = await Promise.all([
+      PhonesModel.find({ brand_name: "apple" }).limit(3),
+      PhonesModel.find({
+        brand_name: "samsung",
+      }).limit(3),
+      PhonesModel.find({ brand_name: "xiaomi" }).limit(3),
+    ]);
+
+    return NextResponse.json({ apple, samsung, xiaomi }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Error al obtener productos destacados" },
+      { status: 500 }
+    );
+  }
+}

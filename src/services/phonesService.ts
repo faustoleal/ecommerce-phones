1fbfc62e14c1268b2ea47d2330ec264f0bc85f0a
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-09 12:21:31 -0300

    agregando fetchProductos

--- src/services/phonesService.ts
+++ src/services/phonesService.ts
@@ -16,3 +16,16 @@ export async function fecthDestacados(): Promise<{
 
   return res.json();
 }
+
+export async function fetchProductos(page: number) {
+  const res = await fetch(`api/phones?page=${page}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!res.ok) {
+    console.log("Error al obtener productos");
+  }
+
+  return res.json();
+}

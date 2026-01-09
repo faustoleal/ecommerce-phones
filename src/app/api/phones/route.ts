ddab346e78352b89c7b2026f5b1aa2e4e405e3ed
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-09 12:19:05 -0300

    modicando peticion GET

--- src/app/api/phones/route.ts
+++ src/app/api/phones/route.ts
@@ -1,7 +1,7 @@
 import { connectDB } from "@/src/lib/db";
 import { listarProductos } from "../../../controllers/phones";
 
-export async function GET() {
+export async function GET(req: Request) {
   connectDB();
-  return listarProductos();
+  return listarProductos(req);
 }

5e0be573568e4a948b1ecc05396baadcd1f0554b
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-10 12:03:20 -0300

    agregando PhoneSection y editando PhoneList

--- src/components/phones/PhoneList.tsx
+++ src/components/phones/PhoneList.tsx
@@ -1,30 +1,51 @@
-"use client";
+import { Phone } from "@/src/types/phones";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
-import { fetchProductos } from "@/src/services/phonesService";
-import { Phones } from "@/src/types/phones";
-import { useEffect, useState } from "react";
+type PhoneListProps = {
+  productos: Phone[];
+};
 
-const PhoneList = () => {
-  const [phones, setPhones] = useState<Phones[]>([]);
-  const [page, setPage] = useState<number>(1);
-
-  useEffect(() => {
-    fetchProductos(page)
-      .then((data) => {
-        setPhones(data.products);
-      })
-      .catch((error) => console.log(error));
-  }, [page]);
-
-  console.log(phones);
-
+const PhoneList: React.FC<PhoneListProps> = ({ productos }) => {
+  console.log(productos);
   return (
-    <div className="lg:col-span-3 space-y-6">
-      {phones.length > 0 ? (
-        <div className=""></div>
-      ) : (
-        <div>No se encontraron productos</div>
-      )}
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+      {productos.map((el) => (
+        <Link key={el._id} href={`/productos/${el._id}`}>
+          <div className="group flex flex-col gap-6 rounded-xl  py-6 shadow-sm overflow-hidden border border-border hover:border-[#6366F1] transition-colors h-full">
+            <div className="p-0">
+              <div className="aspect-square bg-muted relative overflow-hidden">
+                <Image
+                  fill
+                  sizes="{max-width:481px} {max-height: 481px}"
+                  src="/iphone-15-pro-max-titanium.png"
+                  alt={el.model}
+                  loading="eager"
+                  className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+                ></Image>
+              </div>
+              <div className="p-5">
+                <span className="inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-xs mb-2 font-medium w-fit whitespace-nowrap shrink-0 overflow-hidden">
+                  {el.brand_name}
+                </span>
+                <h3 className="font-medium mb-1 text-balance">{el.model}</h3>
+                <p className="text-sm text-muted-foreground mb-3">
+                  {el.internal_memory}GB • {el.ram_capacity} RAM
+                </p>
+                <div className="flex items-center gap-2">
+                  <span className="text-2xl font-medium text-[#6366F1]">
+                    ${el.price}
+                  </span>
+                </div>
+                <p className="text-xs text-muted-foreground mt-2">
+                  Stock: 5 unidades
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
---
+++ src/components/phones/PhoneSection.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { fetchProductos } from "@/src/services/phonesService";
+import { Phone } from "@/src/types/phones";
+import { useEffect, useState } from "react";
+import PhoneList from "./PhoneList";
+
+const PhoneSection = () => {
+  const [phones, setPhones] = useState<Phone[]>([]);
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    fetchProductos(page)
+      .then((data) => {
+        setPhones(data.products);
+      })
+      .catch((error) => console.log(error));
+  }, [page]);
+
+  return (
+    <section className="lg:col-span-3 space-y-6">
+      {phones.length > 0 ? (
+        <PhoneList productos={phones} />
+      ) : (
+        <div className="text-center py-12">
+          <p className="text-muted-foreground mb-4">
+            No se encontraron productos
+          </p>
+          <button className="bg-black text-white border border-border h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium outline-none ">
+            Limpiar filtros
+          </button>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default PhoneSection;

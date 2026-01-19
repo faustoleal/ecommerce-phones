6bfa37ce185aead32bfe4548ce04858401519efa
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-19 10:22:46 -0300

    agregando POST

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,15 @@
-import { getUsuarios } from "@/src/controllers/user";
+import { crearUsuario, getUsuarios } from "@/src/controllers/user";
 import { connectDB } from "@/src/lib/db";
 
 export async function GET() {
   connectDB();
   return getUsuarios();
+}
+
+export async function POST(req: Request) {
+  connectDB();
+  const { name, username, email, password } = await req.json();
+
+  const nuevoUsuario = { name, username, email, password };
+  return crearUsuario(nuevoUsuario);
 }

a81c23454722e46567093636fc7c4e3401b922e9
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-27 12:55:12 -0300

    agregando funcion eliminarItemDelCarrito

--- src/services/carritoService.ts
+++ src/services/carritoService.ts
@@ -38,3 +38,22 @@ export async function limpiarCarrito(id: string) {
 
   return data;
 }
+
+export async function eliminarItemdelCarrito(id: string, productoId: string) {
+  const res = await fetch(`/api/carrito/${id}/${productoId}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  if (data.newToken) {
+    localStorage.setItem("authToken", data.newToken);
+  }
+
+  return data;
+}

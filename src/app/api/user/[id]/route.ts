21c06b8595e6045c106a17727fdfeb4eab492faa
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-27 12:16:05 -0300

    pasando el POST de actualizar carrito a la carpeta api/carrito

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,6 +1,5 @@
-import { acutalizarCarrito, getUserById } from "@/src/controllers/user";
+import { getUserById } from "@/src/controllers/user";
 import { connectDB } from "@/src/lib/db";
-import jwt from "jsonwebtoken";
 
 export async function GET(
   request: Request,
@@ -13,38 +12,3 @@ export async function GET(
   connectDB();
   return getUserById(id);
 }
-
-export async function POST(
-  request: Request,
-  { params }: { params: { id: string } },
-) {
-  connectDB();
-  const { cantidad, productoId } = await request.json();
-  const cart = { productoId, cantidad };
-  const { id } = await params;
-  const response = await acutalizarCarrito(cart, id);
-  
-  // Si la respuesta es exitosa, generar un nuevo token
-  if (response.status === 200) {
-    const data = await response.json();
-    const newToken = jwt.sign(
-      {
-        _id: data._id,
-        name: data.name,
-        username: data.username,
-        email: data.email,
-        role: data.role,
-        carrito: data.carrito,
-      },
-      process.env.SECRET as string,
-    );
-    
-    return Response.json(
-      { ...data, newToken },
-      { status: 200 }
-    );
-  }
-  
-  return response;
-}
-

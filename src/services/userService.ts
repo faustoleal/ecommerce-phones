d7512cb6cf992584004b1511c92fbce387b3765d
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-27 12:19:51 -0300

    pasando agregarAlCarrito a carritoService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import { CartItem, NewUser } from "../types/user";
+import { NewUser } from "../types/user";
 
 export async function registrarUsuario(nuevoUsuario: NewUser | null) {
   const res = await fetch("/api/user", {
@@ -11,26 +11,6 @@ export async function registrarUsuario(nuevoUsuario: NewUser | null) {
 
   if (!res.ok) {
     throw new Error(data.message);
-  }
-
-  return data;
-}
-
-export async function agregarAlCarrito(cart: CartItem, id: string) {
-  const res = await fetch(`/api/user/${id}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(cart),
-  });
-
-  const data = await res.json();
-
-  if (!res.ok) {
-    throw new Error(data.message);
-  }
-
-  if (data.newToken) {
-    localStorage.setItem("authToken", data.newToken);
   }
 
   return data;

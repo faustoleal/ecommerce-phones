0f95d5789b3b681a0df1dffa43bc9af5b80f5ffd
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-27 12:18:49 -0300

    creando el carritoService

---
+++ src/services/carritoService.ts
@@ -0,0 +1,40 @@
+import { CartItem } from "../types/user";
+
+export async function agregarAlCarrito(cart: CartItem, id: string) {
+  const res = await fetch(`/api/carrito/${id}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cart),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  if (data.newToken) {
+    localStorage.setItem("authToken", data.newToken);
+  }
+
+  return data;
+}
+
+export async function limpiarCarrito(id: string) {
+  const res = await fetch(`/api/carrito/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+
+  if (data.newToken) {
+    localStorage.setItem("authToken", data.newToken);
+  }
+
+  return data;
+}

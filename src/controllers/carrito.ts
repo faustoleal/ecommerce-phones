78f0028e27a123ed1903b2e840a5502ea578acbf
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-27 12:54:29 -0300

    agregando funcion eliminarItem

--- src/controllers/carrito.ts
+++ src/controllers/carrito.ts
@@ -65,3 +65,30 @@ export async function vaciarCarrito(id: string) {
     );
   }
 }
+
+export async function eliminarItem(id: string, productoId: string) {
+  try {
+    if (!productoId) {
+      return NextResponse.json({ message: "Datos inválidos" }, { status: 400 });
+    }
+
+    const user = await UserModel.findByIdAndUpdate(id, {
+      $pull: { carrito: { productoId } },
+    });
+
+    await user.save();
+
+    const populateUser = await UserModel.findById(id).populate({
+      path: "carrito.productoId",
+      select: "brand_name price model internal_memory ram_capacity",
+    });
+
+    return NextResponse.json(populateUser, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error al actualizar el carrito" },
+      { status: 500 },
+    );
+  }
+}

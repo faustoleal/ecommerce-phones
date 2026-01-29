b6723cf78bcb3038b01eada5d510c6c6b5954c3b
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-29 12:07:36 -0300

    creando controlador para pedidos

---
+++ src/controllers/pedidos.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { newPedido } from "../types/pedidos";
+import { PedidoModel } from "../models/pedidos";
+
+export async function getPedidos() {
+  try {
+    const pedidos = await PedidoModel.find();
+    if (pedidos.length === 0) {
+      return NextResponse.json(
+        { message: "No hay datos en la base" },
+        { status: 400 },
+      );
+    }
+    return NextResponse.json(pedidos, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Error al obtener pedidos" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function crearPedido(pedido: newPedido) {
+  try {
+    const newPedido = await PedidoModel.create({
+      pedido,
+    });
+
+    return NextResponse.json(
+      {
+        message: "Pedido creado con éxito",
+        newPedido,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error interno del servidor", error },
+      { status: 500 },
+    );
+  }
+}

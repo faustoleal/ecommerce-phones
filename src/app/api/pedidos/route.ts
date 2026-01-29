bd2f34a08f74ea364d33c7ed0159aedd6266ca9b
Fausto Leal <fausto.leal2099@gmail.com>
2026-01-29 12:27:47 -0300

    creando api endpoint /api/pedidos

---
+++ src/app/api/pedidos/route.ts
@@ -0,0 +1,15 @@
+import { crearPedido, getPedidos } from "@/src/controllers/pedidos";
+import { connectDB } from "@/src/lib/db";
+
+export async function GET() {
+  connectDB();
+  return getPedidos();
+}
+
+export async function POST(req: Request) {
+  connectDB();
+  const { orderNum, productos, user, status, date } = await req.json();
+
+  const nuevoPedido = { orderNum, productos, user, status, date };
+  return crearPedido(nuevoPedido);
+}

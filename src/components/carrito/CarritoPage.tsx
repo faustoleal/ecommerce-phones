0222898cb116f1f8c42f0220727c882885bed21f
Fausto Leal <fausto.leal2099@gmail.com>
2026-02-11 12:01:48 -0300

    modifcando CarritoPage.tsx

--- src/components/carrito/CarritoPage.tsx
+++ src/components/carrito/CarritoPage.tsx
@@ -2,7 +2,7 @@
 
 import { useApp } from "@/src/context/UserContext";
 import Button from "../Button";
-import { ArrowLeft, Minus, Plus, ShoppingBag, Trash2 } from "lucide-react";
+import { ArrowLeft, Minus, Plus, Trash2 } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";

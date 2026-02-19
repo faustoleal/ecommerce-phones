aa153bb888a784e99432ccc27a37e20b438ca377
Fausto Leal <fausto.leal2099@gmail.com>
2026-02-19 11:43:22 -0300

    modificanfo funcion GET

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -2,12 +2,10 @@ import { getUserById } from "@/src/controllers/user";
 import { connectDB } from "@/src/lib/db";
 
 export async function GET(
-  request: Request,
+  req: Request,
   { params }: { params: { id: string } },
 ) {
   const { id } = await params;
-
-  console.log(id);
 
   connectDB();
   return getUserById(id);

5658a3c6fcc0a77d7508b635543ed3a7dce7fdde
Fausto Leal <fausto.leal2099@gmail.com>
2026-02-24 12:24:05 -0300

    agregando metodo POST a la ruta api/phones

--- src/app/api/phones/route.ts
+++ src/app/api/phones/route.ts
@@ -1,7 +1,62 @@
 import { connectDB } from "@/src/lib/db";
-import { listarProductos } from "../../../controllers/phones";
+import { listarProductos, postProducto } from "../../../controllers/phones";
 
 export async function GET(req: Request) {
   connectDB();
   return listarProductos(req);
+}
+
+export async function POST(req: Request) {
+  connectDB();
+  const {
+    model,
+    brand_name,
+    price,
+    rating,
+    processor_brand,
+    processor_speed,
+    num_cores,
+    os,
+    internal_memory,
+    ram_capacity,
+    extended_memory_available,
+    screen_size,
+    resolution_width,
+    resolution_height,
+    battery_capacity,
+    fast_charging_available,
+    num_front_cameras,
+    primary_camera_front,
+    num_rear_cameras,
+    primary_camera_rear,
+    has_5g,
+    has_nfc,
+    has_ir_blaster,
+  } = await req.json();
+
+  return postProducto({
+    model,
+    brand_name,
+    price,
+    rating,
+    processor_brand,
+    processor_speed,
+    num_cores,
+    os,
+    internal_memory,
+    ram_capacity,
+    extended_memory_available,
+    screen_size,
+    resolution_width,
+    resolution_height,
+    battery_capacity,
+    fast_charging_available,
+    num_front_cameras,
+    primary_camera_front,
+    num_rear_cameras,
+    primary_camera_rear,
+    has_5g,
+    has_nfc,
+    has_ir_blaster,
+  });
 }

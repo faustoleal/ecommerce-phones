474a19c836fe76b2201d2280609c3e3a0089647e
Fausto Leal <fausto.leal2099@gmail.com>
2026-03-11 10:54:39 -0300

    cambiando meta descriptiton de la página productos

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Productos | Ecommerce Phones",
   description:
-    "Descubre la mejor selección de celulares Apple, Samsung, Xiaomi y más en Ecommerce Phones. Envíos rápidos y garantía oficial.",
+    "Descubre nuestra amplia selección de smartphones en Ecommerce Phones. Encuentra las últimas marcas y modelos con precios competitivos, especificaciones detalladas y opciones de compra seguras. Explora y elige el teléfono ideal para ti.",
 };
 
 export default function Productos() {

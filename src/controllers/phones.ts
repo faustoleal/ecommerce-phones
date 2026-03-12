8be05c961386e2a6fdc5079660dd06b313239ca3
Fausto Leal <fausto.leal2099@gmail.com>
2026-03-12 12:03:56 -0300

    agregando queryMap y filters a GET

--- src/controllers/phones.ts
+++ src/controllers/phones.ts
@@ -5,12 +5,39 @@ import { NewProduct } from "../types/phones";
 export async function getPhones(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
+    console.log(req.url);
     const page = parseInt(searchParams.get("page") || "1", 10);
 
     const limit = 12;
     const skip = (page - 1) * limit;
 
-    const products = await PhonesModel.find().skip(skip).limit(limit);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const queryMap: Record<string, (val: string) => any> = {
+      minPrice: (val) => ({ price: { $gte: Number(val) } }),
+      maxPrice: (val) => ({ price: { $lte: Number(val) } }),
+      internalMemory: (val) => ({ internal_memory: { $gte: Number(val) } }),
+      has5g: (val) => ({ has_5g: val === "true" }),
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const filters: any = {};
+
+    searchParams.forEach((value, key) => {
+      if (queryMap[key]) {
+        const newFilter = queryMap[key](value);
+
+        // Si ya existe el campo en filters, mergea operadores
+        Object.keys(newFilter).forEach((field) => {
+          if (filters[field]) {
+            filters[field] = { ...filters[field], ...newFilter[field] };
+          } else {
+            filters[field] = newFilter[field];
+          }
+        });
+      }
+    });
+
+    const products = await PhonesModel.find(filters).skip(skip).limit(limit);
 
     const total = await PhonesModel.countDocuments();
     const totalPages = Math.ceil(total / limit);

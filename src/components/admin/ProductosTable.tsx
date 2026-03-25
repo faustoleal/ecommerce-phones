e684e018cad6817c4c6507bad1ba81c0590cf775
Fausto Leal <fausto.leal2099@gmail.com>
2026-03-25 11:59:16 -0300

    pasando totalFilter al Pagination

--- src/components/admin/ProductosTable.tsx
+++ src/components/admin/ProductosTable.tsx
@@ -96,6 +96,7 @@ const ProductosTable: React.FC<ProductosTableProps> = ({
             page={productos.page}
             totalPages={productos.totalPages}
             totalPhones={productos.totalPhones}
+            totalFilter={productos.totalFilter}
             setPage={setPage}
           />
         </CardContent>

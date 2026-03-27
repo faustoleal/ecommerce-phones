7f1371000f0cca9deaac89859738fd80b29d1ada
Fausto Leal <fausto.leal2099@gmail.com>
2026-03-27 11:35:40 -0300

    agregadno la funcion serializeFilters y usando en el useEffect

--- src/components/views/ProductosPage.tsx
+++ src/components/views/ProductosPage.tsx
@@ -1,8 +1,73 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import FilterSection from "../productos/FilterSection";
 import PhoneSection from "../productos/PhoneSection";
 import { Filter } from "lucide-react";
+import { fetchProductos } from "@/src/services/phonesService";
+import { Phone } from "@/src/types/phones";
+
+export interface ProductosState {
+  page: number;
+  totalPages: number;
+  totalPhones: number;
+  totalFilter: number;
+  products: Phone[];
+}
+
+export interface Filtros {
+  brands?: string[];
+  maxPrice?: number;
+  fastCharging?: boolean;
+  ramCapacity?: number;
+  primaryCameraRear?: { min: number; max: number };
+  primaryCameraFront?: { min: number; max: number };
+  extendedMemory?: boolean;
+  internalMemory?: number;
+  has5g?: boolean;
+}
 
 const ProductosPage = () => {
+  const [productos, setProductos] = useState<ProductosState>({
+    page: 0,
+    totalPages: 0,
+    totalPhones: 0,
+    totalFilter: 0,
+    products: [],
+  });
+  const [filters, setFilters] = useState<Filtros>({});
+  const [page, setPage] = useState<number>(1);
+
+  const serializeFilters = (filters: Filtros): URLSearchParams => {
+    const params = new URLSearchParams();
+
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value === undefined || value === null) return;
+
+      if (Array.isArray(value)) {
+        params.append(key, value.join(","));
+      } else if (typeof value === "object") {
+        const { min, max } = value as { min: number; max: number };
+        params.append(key, `${min},${max}`);
+      } else {
+        params.append(key, String(value));
+      }
+    });
+
+    return params;
+  };
+
+  useEffect(() => {
+    const params = serializeFilters(filters);
+    const queryString = params.toString();
+
+    fetchProductos(page, queryString)
+      .then((data) => {
+        setProductos(data);
+      })
+      .catch((error) => console.log(error));
+  }, [page, filters]);
+
   return (
     <div className="min-h-screen py-12">
       <div className="container mx-auto px-5">
@@ -18,10 +83,10 @@ const ProductosPage = () => {
                 <h2 className="font-medium">Filtros</h2>
                 <Filter className="h-4 w-4" />
               </div>
-              <FilterSection />
+              <FilterSection filters={filters} setFilters={setFilters} />
             </div>
           </aside>
-          <PhoneSection />
+          <PhoneSection productos={productos} setPage={setPage} />
         </div>
       </div>
     </div>
